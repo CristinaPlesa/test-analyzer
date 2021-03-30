26b03f07b3114c72f3fc03146083964de145fbc6
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-03-30 13:34:57 -0700

    add html
    Co-authored-by: Jeremy Banka <jeremybanka@gmail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
+// business logic
 function wordCounter(text) {
-  const stringIsEmpty = text.trim().length === 0
-  if (stringIsEmpty) return 0
+  const stringIsEmpty = text.trim().length === 0;
+  if (stringIsEmpty) return 0;
   let wordCount = 0;
   const elements = text.split(" ");
   elements.forEach(function (element) {
-    const elementIsntNumber = Number.isNaN(Number(element))
+    const elementIsntNumber = Number.isNaN(Number(element));
     if (elementIsntNumber) wordCount++;
-
   });
   return wordCount;
 }
-
-// business logic
 
 function numberOfOccurrencesInText(word, text) {
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function (element) {
     if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++
+      wordCount++;
     }
-  })
+  });
   return wordCount;
 }
-console.log(numberOfOccurrencesInText('red', 'red'))
+console.log(numberOfOccurrencesInText("red", "red"));
